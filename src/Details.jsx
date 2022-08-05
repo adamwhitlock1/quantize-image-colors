a5065ffd0761b97f4bacbfae65fb51a7e4d0add1
Adam Whitlock <adam.whitlock@adhocteam.us>
2022-08-05 14:53:23 -0600

    add home button bc codesandbox sux

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,4 +1,5 @@
-import { useParams, useLocation } from "react-router-dom";
+import { useParams, useLocation, Link } from "react-router-dom";
+import { tw } from "twind";
 
 const Details = () => {
   const { id } = useParams();
@@ -6,12 +7,20 @@ const Details = () => {
   const location = useLocation();
 
   return (
-    <>
-      <h1>Details</h1>
-      <p>id: {id}</p>
+    <div className={tw`p-4`}>
+      <Link
+        className={tw`p-2 rounded border-2 border-blue-500 mb-4 text-blue-500`}
+        to="/"
+      >
+        Go back to home
+      </Link>
+      <div className={tw`mt-2`}>
+        <h1>Details</h1>
+        <p>id: {id}</p>
+      </div>
 
       <pre>{JSON.stringify(location?.state, null, 2)}</pre>
-    </>
+    </div>
   );
 };
 
